d8023d57e74b90d5d9218861d21807ebec12e9b0
river.taewoo <cooltwk@naver.com>
2022-09-25 06:25:55 +0000

    setup fbase.js

---
+++ WEB(FE)/src/fbase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID
+};
+
+export default firebase.initializeApp(firebaseConfig);
