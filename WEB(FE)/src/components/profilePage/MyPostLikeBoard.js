4a5ebc71dd97a86584b5fb9df1ef5ae12cd2cda7
river.taewoo <cooltwk@naver.com>
2022-10-10 01:06:45 +0000

    Woring on MyPostLikeBoard.js

--- WEB(FE)/src/components/profilePage/MyPostLikeBoard.js
+++ WEB(FE)/src/components/profilePage/MyPostLikeBoard.js
@@ -12,18 +12,7 @@ const MyPostLikeBoard = () => {
 	const [nextItemSnapShot, setNextItemSnapShot] = useState({});
   const [isNextItemExist, setIsNextItemExist] = useState(false);
 
-	const snapShotToLikedPosts = useCallback((snapshot) => {
-
-	})
-	const myPostLikeBoard = async (nowUserId) => {
-    console.log("직전 아이디: ", nowUserId);
-    const q = query(collection(dbService, "PostLike"),
-      where("user_id", "==", nowUserId),
-      orderBy("created_timestamp", "desc")
-    )
-    const snapshot = await getDocs(q);
-    console.log("SNAPSHOT:", snapshot.docs);
-    
+  const snapShotToLikedPosts = useCallback((snapshot) => {
     if (snapshot) {
       snapshot.forEach(async (document) => {
         const commentLikeObj = {
@@ -35,16 +24,66 @@ const MyPostLikeBoard = () => {
         const postSnap = await getDoc(postRef)
         const postLikedObj = {
           ...postSnap.data(),
-          id:postSnap.id,
+          id: postSnap.id,
         }
         setPostsLiked((prev) => [...prev, postLikedObj])
       })
     }
+  }, []);
+
+	const myPostLikeBoard = async (next) => {
+		if (next) {
+      console.log("showing next liked posts");
+      const querySnapshot = await getDocs(
+				query(collection(dbService, "PostLike"),
+					orderBy("created_timestamp", "desc"),
+					where("user_id", "==", currentUserUid),
+					startAfter(nextItemSnapShot),
+					limit(10)
+				)
+      );
+      setNextItemSnapShot(querySnapshot.docs[querySnapshot.docs.length - 1]);
+      
+      const afterSnapshot = await getDocs(
+				query(collection(dbService, "PostLike"),
+					orderBy("created_timestamp", "desc"),
+					where("user_id", "==", currentUserUid),
+					startAfter(querySnapshot.docs[querySnapshot.docs.length - 1]),
+					limit(1)
+				)
+      );
+      if (afterSnapshot.docs.length === 0) {
+				setIsNextItemExist(false);
+			} else {
+				setIsNextItemExist(true);
+      };
+			snapShotToLikedPosts(querySnapshot);
+		} else {
+			const firstSnapshot = await getDocs(
+				query(collection(dbService, "PostLike"),
+					where("user_id", "==", currentUserUid),
+					orderBy("created_timestamp", "desc"),
+					limit(10)
+				)
+			);
+			setNextItemSnapShot(firstSnapshot.docs[firstSnapshot.docs.length - 1]);
+			snapShotToLikedPosts(firstSnapshot);
+			if(firstSnapshot.docs.length < 10) {
+				setIsNextItemExist(false);
+			} else {
+				setIsNextItemExist(true);
+			}
+		}
   }
 
+	const onClick = async (e) => {
+		e.preventDefault();
+		myPostLikeBoard(true);
+	}
 	useEffect(() => {
-		myPostLikeBoard(currentUserUid);
-	}, [])
+		myPostLikeBoard(false);
+  }, [])
+  
 	return (
 		<div><h4>공감한 고민 글</h4> <hr />
 			{postsLiked.length !== 0 ? (
@@ -56,6 +95,9 @@ const MyPostLikeBoard = () => {
 					))
 				) : (
 					<div>잠시만 기다려 주세요</div>
+			)}
+      {isNextItemExist && (
+				<button onClick={onClick}>내 포스트 10개 더 보기</button>
 			)}
 			<br />
 		</div>
