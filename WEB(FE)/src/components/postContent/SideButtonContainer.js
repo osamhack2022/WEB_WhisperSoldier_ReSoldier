74871287abd7666b3dd87795ff1a3f3d652b7594
river.taewoo <cooltwk@naver.com>
2022-10-13 13:12:48 +0000

    starting working on adding chat pairs

--- WEB(FE)/src/components/postContent/SideButtonContainer.js
+++ WEB(FE)/src/components/postContent/SideButtonContainer.js
@@ -1,3 +1,4 @@
+import { dbFunction, dbService } from "../../lib/FStore";
 import {
   DeletePostButton,
   EditPostButton,
@@ -34,6 +35,25 @@ export const OtherUserButtonContainer = ({
   toggleLike,
   isLikedByMe,
 }) => {
+  const onClickChatButtonFromPost = async (e) => {
+    const { query, addDoc, collection, orderBy, onSnapshot, where, getDocs } = dbFunction;
+    //채팅방이 이미 존재하는지 체크하기
+    const res = await getDocs(dbService, "ChatPair");
+    //만약 없다면, 새로 만들기
+    //있다면, 일단 채팅페이지로 navigate
+    //서브컬렉션도 이 단계에서 만들어줘야되는건가...?? 아닌가? 알아봐야됨
+    //밑에 있는 예시 기반으로 문서 추가 예정
+    await addDoc(collection(dbService, "Comment"), {
+
+      /* commentor_id: authService.currentUser.uid,
+      associated_post_id: postInfo.id,
+      comment_text: state.comment,
+      comment_report: false,
+      comment_rep_accept: false,
+      like_count: 0,
+      created_timestamp: serverTimestamp(), */
+    });
+  };
   return (
     <>
       <LikeButton
