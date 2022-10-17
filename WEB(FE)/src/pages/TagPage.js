58595a6436b6dbbd5fe19df61f3c7b7e6b80bac0
river.taewoo <cooltwk@naver.com>
2022-10-17 16:31:41 +0000

    implemented tag select and fixed bugs

--- WEB(FE)/src/pages/TagPage.js
+++ WEB(FE)/src/pages/TagPage.js
@@ -1,14 +1,17 @@
 import { collection, getDocs, limit, orderBy, query, startAfter, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { dbService } from "../lib/FStore";
 
 
 const TagPage = () => {
 
 	const [tags, setTags] = useState([]);
+	const [selectedTag, setSelectedTag] = useState("");
+	const [tagPosts, setTagPosts] = useState([]);
   const [nextTagSnapshot, setNextTagSnapshot] = useState({});
+  const [nextPostSnapshot, setNextPostSnapshot] = useState({});
   const [isNextTagExist, setIsNextTagExist] = useState(false);
-  const [isShowContainer, setIsShowContainer] = useState(false);
 
 	const snapshotToTags = (snapshot) => {
     if (snapshot) {
@@ -19,6 +22,19 @@ const TagPage = () => {
         };
         console.log(tagObj);
         setTags((prev) => [...prev, tagObj]);
+      });
+    }
+	};
+
+	const snapshotToPosts = (snapshot) => {
+    if (snapshot) {
+      snapshot.forEach((doc) => {
+        const postObj = {
+          ...doc.data(),
+          id: doc.id,
+        };
+        console.log(postObj);
+        setTagPosts((prev) => [...prev, postObj]);
       });
     }
 	};
@@ -81,10 +97,49 @@ const TagPage = () => {
 			}
 			snapshotToTags(querySnapshot);
 		};
-		const onClick = async (e) => {
+		const onClickForMore = async (e) => {
 			e.preventDefault();
 			moveNext();
 		};
+	
+	const getTagPosts = async (tagName) => {
+		const firstSnapshot = await getDocs(
+			query(collection(dbService, "WorryPost"),
+				orderBy("created_timestamp", "desc"),
+				where("tag_name", "==", tagName),
+				limit(10),
+			)
+		);
+		setNextPostSnapshot(firstSnapshot.docs[firstSnapshot.docs.length - 1]);
+		snapshotToPosts(firstSnapshot);
+		if (firstSnapshot.docs.length < 10) {
+		  setIsNextTagExist(false);
+		} else {
+		  try {
+			const nextTagsSnapshot = await getDocs(
+				query(collection(dbService, "WorryPost"),
+				orderBy("created_timestamp", "desc"),
+				where("tag_name", "==", tagName),
+				limit(10),
+			)
+			);
+			if (nextTagsSnapshot.docs.length === 0) {
+			  setIsNextTagExist(false);
+			} else {
+			  setIsNextTagExist(true);
+			}
+		  } catch (e) {
+			console.log("Error with getting tags!");
+		  }
+		}
+	}
+	
+	const selectTag = async (tagName) => {
+		setSelectedTag(tagName);
+		setTagPosts([]);
+		console.log("tagName: ", tagName);
+		getTagPosts(tagName);
+	}
 	useEffect(() => {
 		getFirst();
 	}, [])
@@ -93,17 +148,29 @@ const TagPage = () => {
 			<div>태그 페이지</div>
 			{tags.length !== 0 ? (
               tags.map((tagdoc) => (
-								<div key={tagdoc.id} tagdoc={tagdoc}>#{tagdoc.tag_name}, {tagdoc.tag_count} More</div>
+								<div key={tagdoc.id} tagdoc={tagdoc} onClick={() => selectTag(tagdoc.tag_name)}>#{tagdoc.tag_name}, {tagdoc.tag_count} More</div>
               ))
             ) : (
               <div>잠시만 기다려 주세요</div>
 			)}
 			{isNextTagExist && (
-        <button onClick={onClick}>20개 더 보기</button>
+        <button onClick={onClickForMore}>20개 더 보기</button>
 			)}
-			<div>해당 태그를 가진 Tag들</div>
 			<div>
-
+				{selectedTag === "" ? <>태그를 선택해 주세요: {selectedTag}</> :
+					<>해당 태그를 가진 Post들</>
+				}
+				{tagPosts.length !== 0 ? (
+              tagPosts.map((tagpost) => (
+								<div key={tagpost.id}>
+									<br />
+									<br />
+									<Link  to={`/post/${tagpost.id}`}>{tagpost.text}</Link>
+								</div>
+              ))
+            ) : (
+              <div>포스트를 불러오는 중이거나 해당 태그의 포스트가 존재하지 않습니다.</div>
+			)}
 			</div>
 		</>
 
